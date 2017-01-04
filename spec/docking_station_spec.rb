bf53970b848a1d58497789105b409f255a3b9af8
John Ashton <John_ashton@hotmail.com>
2017-01-04 15:23:47 +0000

    Exercise 9

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,5 @@ describe DockingStation do
   #it 'responds to release bike' do
   #  expect(DockingStation.new.respond_to?(:release_bike)).to eq true
   #end
-  subject{DockingStation.new}
   it {is_expected.to respond_to(:release_bike)}
 end
