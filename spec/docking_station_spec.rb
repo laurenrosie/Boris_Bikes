f032f7063973070495ee2995a7c77996d2d066ec
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-01-04 15:37:52 +0000

    Exercise 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,14 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   #it 'responds to release bike' do
   #  expect(DockingStation.new.respond_to?(:release_bike)).to eq true
   #end
-  it {is_expected.to respond_to(:release_bike)}
+  it{is_expected.to respond_to(:release_bike)}
+
+  it 'expects bike to be working' do
+    #does release bike expect the bike to be working
+    expect(DockingStation.new.release_bike.working?).to eq(true)
+  end
 end
