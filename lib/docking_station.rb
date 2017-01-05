c0531f1f6c6cd066903688ede737c7e9e2369aeb
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-01-05 13:50:59 +0000

    refactoring

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,12 +4,14 @@ class DockingStation
   def initialize(has_bike=true)
     @has_bike = has_bike
   end
+  
   def release_bike
     @has_bike=false
     Bike.new
   end
-  def dock(bike_name)
-    bike_name.docked=(true)
+
+  def dock_bike(this_bike)
+    this_bike.docked=(true)
     @has_bike=true
   end
 end
