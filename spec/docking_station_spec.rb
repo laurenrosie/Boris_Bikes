3f398d00dac616e325f1afeee56808702426d189
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-01-05 13:54:27 +0000

    Change spec to use one liner syntax for all tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,5 @@ require 'bike'
 describe DockingStation do
   it{is_expected.to respond_to(:release_bike)}
   it{is_expected.to respond_to(:dock_bike)}
-
-   it 'expects bike to be working' do
-     expect(subject.release_bike.working).to eq(true)
-   end
-
+  it {expect(subject.release_bike.working).to be_truthy}
 end
